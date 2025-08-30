cc68391862c99a446b4dcec3c95385415773e34f
AryanJain991 <168620401+AryanJain991@users.noreply.github.com>
2025-08-30 15:25:56 +0530

    Regular Expression

--- PYTHON FOR BEGINNERS BY HARRY/chapter14_external_modules/02_requests.py
+++ PYTHON FOR BEGINNERS BY HARRY/chapter14_external_modules/02_requests.py
@@ -2,4 +2,7 @@ import requests
 
 r = requests.get('https://api.github.com/users/aryanjain991')
 
-print(r.text)
+# print(r.text)
+
+with open("Api_fetch_details.txt", "w") as f:
+    f.write(r.text)
